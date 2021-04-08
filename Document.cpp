075499e8b6c85a7a45e1b7b686055ff788a0c526
dekel241 <dekel241@gmail.com>
2021-04-09 01:16:47 +0300

    Fix comment

--- Document.cpp
+++ Document.cpp
@@ -148,7 +148,7 @@ string Document::sed_replace(const string & old_text, const string & new_text)
 {
     string result = "?";
     regex pattern(old_text);
-    //Temp uses in order to later overwrite the new value
+    //Temp uses as placeholder in order to later overwrite the new value
     string temp = regex_replace(_lines[_current_line - 1], pattern, new_text);
     result = (temp == _lines[_current_line - 1]) ? result : temp;
     _lines[_current_line - 1] = temp; //regex doesn't overwrite the input line
